291c3f8c09950e84fbfe3d96d63bddca24f93c40
Kai Tebay <kaitebay@protonmail.com>
2022-09-26 14:33:38 +0100

    Updated MouseAcceleration.sh comment description

--- MouseAcceleration.sh
+++ MouseAcceleration.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# A script to disable mouse acceleration.
+# A script to disable mouse acceleration in GNOME.
 
 # Use gsettings to set the mouse acceleration profile to "flat".
 gsettings set org.gnome.desktop.peripherals.mouse accel-profile flat
