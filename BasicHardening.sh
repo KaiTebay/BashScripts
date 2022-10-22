abb4a2b5c82c77d7d4fd601dbac31b1276daf0ae
KaiTebay <kaitebay@protonmail.com>
2022-10-22 08:49:32 +0100

    Removed BasicHardening.sh

--- BasicHardening.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# A script to configure system settings for improved security.
-
-# Update umask to make new files unreadable by anyone other than the owner.
-echo "umask 0077" >> /etc/profile
