779feb88059bebdd18aa98bd940b13b7ae1c73a2
KaiTebay <kaitebay@protonmail.com>
2022-10-25 10:51:49 +0100

    Updated Combination-Audio-ALSA.sh

--- Combination-Audio-ALSA.sh
+++ Combination-Audio-ALSA.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # A script to congfigure ALSA to recognize combination audio jacks.
 
-echo "options snd_hda_intel index=0 model=dell-headset-multi" | tee /etc/modprobe.d/alsa-base.conf
+echo "options snd_hda_intel index=0 model=headset-mode" | tee /etc/modprobe.d/alsa-base.conf
